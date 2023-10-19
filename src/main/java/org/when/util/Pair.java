d5559ca10fb166b206883c5cdda079572bf14c60
wen <knowhen@outlook.com>
2023-10-19 15:31:28 +0800

    Add a new util class Pair

---
+++ src/main/java/org/when/util/Pair.java
@@ -0,0 +1,35 @@
+package org.when.util;
+
+import java.util.Objects;
+
+public class Pair<L, R> {
+
+    public final L left;
+    public final R right;
+
+    public Pair(L left, R right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    public String toString() {
+        return "Pair[" + left + "," + right + "]";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pair<?, ?> pair = (Pair<?, ?>) o;
+        return Objects.equals(left, pair.left) && Objects.equals(right, pair.right);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(left, right);
+    }
+
+    public static <L, R> Pair<L, R> of(L left, R right) {
+        return new Pair<>(left, right);
+    }
+}
